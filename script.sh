024ce9ca8cacbfab48ebf08d6d59cad036e9dc18
Lucas Canavosio <84486973+lcanavosio@users.noreply.github.com>
2022-05-20 23:55:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 echo This is a test
 echo option.var1 is @option.var1@
 echo option.var2 is @option.var2@
-echo data.var1 is @data.var1*@
-echo data.var2 is @data.var2*@
+echo data.var1* is @data.var1*@
+echo data.var2* is @data.var2*@
 echo export.var1 is @export.var1@
 echo export.var2 is @export.var2@
 echo globals.var1 is @globals.var1@
