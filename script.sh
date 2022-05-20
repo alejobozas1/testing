a4af280083b4451ab62d8630e38a77467d998936
Lucas Canavosio <84486973+lcanavosio@users.noreply.github.com>
2022-05-20 17:26:45 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ echo data.var1 is @data.var1*@
 echo data.var2 is @data.var2*@
 echo export.var1 is @export.var1*@
 echo export.var2 is @export.var2*@
-echo globals.var1 is @globals.var1*@
-echo globals.var2 is @globals.var2*@
+echo globals.var1 is @globals.var1@
+echo globals.var2 is @globals.var2@
