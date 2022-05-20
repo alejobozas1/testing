22830070be955b8ebfbf7f7a8f72c9227197bd35
Lucas Canavosio <84486973+lcanavosio@users.noreply.github.com>
2022-05-20 23:52:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ echo option.var1 is @option.var1@
 echo option.var2 is @option.var2@
 echo data.var1 is @data.var1*@
 echo data.var2 is @data.var2*@
-echo export.var1 is @export.var1*@
-echo export.var2 is @export.var2*@
+echo export.var1 is @export.var1@
+echo export.var2 is @export.var2@
 echo globals.var1 is @globals.var1@
 echo globals.var2 is @globals.var2@
 echo RD_OPTION_VAR1 is $RD_OPTION_VAR1
