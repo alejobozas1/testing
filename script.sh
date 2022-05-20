dbd76d2f80eb2e725285e1ae5f05a7874f9ea466
Lucas Canavosio <84486973+lcanavosio@users.noreply.github.com>
2022-05-20 15:18:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-echo hello
+#!/bin/sh
+echo This is a test
+echo the job name is @job.name@
+echo var1 value is @globals.var1@
+echo var2 value is @globals.var2@
+echo key-value data captured was @data.variable@
+echo key-value data converted to global was @export.variable@
